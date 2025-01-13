bde905c021b2dbf762de3b495f49601f7f23bec3
iamsuru <suryanshshrivastava6364@gmail.com>
2025-01-13 23:37:46 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <></>
   );
 }
 
