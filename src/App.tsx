f0e0c7b97e83d1c01f6190a12af2e57e3d444b80
iamsuru <suryanshshrivastava6364@gmail.com>
2025-02-13 23:49:09 +0530

    feat: added SEO

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { HomeScreen } from "./pages/HomeScreen";
 import { NavBar } from "./pages/NavBar";
+import { Helmet } from 'react-helmet';
 import "./styles/app.css";
 import { ForgotPasswordPage } from "./pages/ForgotPassword";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -9,6 +10,17 @@ import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <>
+      <Helmet>
+        <meta name="description" content="PassMate: Secure and easy password manager for your digital life." />
+        <meta name="keywords" content="password manager, secure passwords, digital life, password protection, passmate, passmate netlify, web-passmate, web passmate, passmate web, password storer, password keeper" />
+        <meta name="author" content="Suryansh Shrivastava" />
+        <meta property="og:title" content="PassMate - Your Password Manager" />
+        <meta property="og:description" content="Store your passwords securely and manage them effortlessly with PassMate." />
+        <meta property="og:url" content="https://web-passmate.netlify.app" />
+        <meta name="linkedin:creator" content="@iamsuru" />
+        <title>PassMate - Secure Password Manager</title>
+      </Helmet>
+
       <NavBar />
       <Routes>
         <Route
